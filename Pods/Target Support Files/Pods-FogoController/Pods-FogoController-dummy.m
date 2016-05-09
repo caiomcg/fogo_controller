dd8f6cb0ae3e4052994fd2e15d7e61af260aba05
caio_guedes <caiomcg@gmail.com>
2016-05-09 16:18:21 -0300

    Initialize an integrated workspace with Alamofire

---
+++ Pods/Target Support Files/Pods-FogoController/Pods-FogoController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FogoController : NSObject
+@end
+@implementation PodsDummy_Pods_FogoController
+@end
---
+++ Pods/Target Support Files/Pods-FogoController/Pods-FogoController-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FogoControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FogoControllerVersionString[];
+
